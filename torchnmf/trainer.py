389e03983bfecb332a4ae8743c3207fcd16acba4
Ankit Khambhati <akhambhati@gmail.com>
2022-01-21 12:25:26 -0800

    feat: Added comments for clarity. Removed gamma multiplier

--- torchnmf/trainer.py
+++ torchnmf/trainer.py
@@ -52,13 +52,14 @@ class BetaMu(Optimizer):
         # Make sure the closure is always called with grad enabled
         closure = torch.enable_grad()(closure)
 
+        # Cache the gradient status for reversion at the end of the function
         status_cache = dict()
-
         for group in self.param_groups:
             for p in group['params']:
                 status_cache[id(p)] = p.requires_grad
                 p.requires_grad = False
 
+        # Iterate over each parameter group (specifies order of optimization)
         for group in self.param_groups:
             beta = group['beta']
             l1_reg = group['l1_reg']
@@ -66,23 +67,25 @@ class BetaMu(Optimizer):
             ortho = group['orthogonal']
             thetas = group['thetas']
 
-            if beta < 1:
-                gamma = 1 / (2 - beta)
-            elif beta > 2:
-                gamma = 1 / (beta - 1)
-            else:
-                gamma = 1
-
+            # Iterate over model parameters within the group
+            # if a gradient is not required then that parameter is "fixed"
             for p in group['params']:
                 if not status_cache[id(p)]:
                     continue
                 p.requires_grad = True
 
+                # Close the optimization loop by retrieving the
+                # observed data and prediction
                 V, WH = closure()
                 if not WH.requires_grad:
                     p.requires_grad = False
                     continue
 
+                # Multiplicative update coefficients for beta-divergence
+                #      Marmin, A., Goulart, J.H.D.M. and Févotte, C., 2021.
+                #      Joint Majorization-Minimization for Nonnegative Matrix
+                #      Factorization with the $\beta $-divergence. 
+                #      arXiv preprint arXiv:2106.15214.
                 if beta == 2:
                     output_neg = V
                     output_pos = WH
@@ -97,25 +100,31 @@ class BetaMu(Optimizer):
                     WH_eps = WH.add(eps)
                     output_neg = WH_eps.pow(beta - 2).mul_(V)
                     output_pos = WH_eps.pow_(beta - 1)
-                # first backward
+
+                # Numerator (negative factor) gradient
                 WH.backward(output_neg, retain_graph=True)
                 neg = torch.clone(p.grad).relu_()
                 p.grad.zero_()
+
+                # Denominator (positive factor) gradient
                 WH.backward(output_pos)
                 pos = torch.clone(p.grad).relu_()
                 p.grad.add_(-neg)
 
+                # Add regularizers to the denominator factor
                 if l1_reg > 0:
                     pos.add_(l1_reg)
                 if l2_reg > 0:
                     pos.add_(p, alpha=l2_reg)
-
                 if ortho > 0:
                     pos.add_(p.sum(1, keepdims=True) - p, alpha=ortho)
 
+                # Avoid ill-conditioned, zero-valued multipliers
                 pos.add_(eps)
                 neg.add_(eps)
 
+                # Cache the multiplicative update numerator/denominator as state
+                # variables within the optimizer. Enables incremental learning.
                 # TODO: Lazy state initialization, should init in constructor
                 state = self.state[p]
                 if len(state) == 0:
@@ -123,20 +132,17 @@ class BetaMu(Optimizer):
                     state['neg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
                     state['pos'] = torch.zeros_like(p, memory_format=torch.preserve_format)
 
-                if thetas[0] > 0:
-                    neg = neg * (p ** (1/gamma))
-
-                #
+                # Accumulate gradients 
                 state['neg'] = state['neg']*thetas[0] + neg*thetas[1]
                 state['pos'] = state['pos']*thetas[0] + pos*thetas[1]
                 state['step'] += 1
 
-                multiplier = state['neg'] / state['pos']
-                if gamma != 1:
-                    multiplier.pow_(gamma)
+                # Compute the current state of the parameter
+                p.data = p.data * (state['neg'] / state['pos'])
 
+                # If gradients are accumulated, then ensure the parameters
+                # are normalized and the gradients are also re-scaled
                 if thetas[0] > 0:
-                    p.data = multiplier
 
                     # TODO: Iterator should reference a rank parameter rather than
                     # matrix shape.
@@ -148,10 +154,9 @@ class BetaMu(Optimizer):
                         p[:,r] = p[:,r] / norm
                         state['neg'][:,r] = state['neg'][:,r] / norm
                         state['pos'][:,r] = state['pos'][:,r] * norm
-                else:
-                    p.mul_(multiplier)
                 p.requires_grad = False
 
+        # Reinstate the grad status from before
         for group in self.param_groups:
             for p in group['params']:
                 p.requires_grad = status_cache[id(p)]
