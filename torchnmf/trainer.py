cd347b4b4c4f3f134141c592ea1ddb128df9e2e4
Ankit Khambhati <akhambhati@gmail.com>
2022-01-31 01:04:13 -0800

    Added  and  arguments for the trainer

--- torchnmf/trainer.py
+++ torchnmf/trainer.py
@@ -21,9 +21,13 @@ class BetaMu(Optimizer):
         orthogonal (float, optional): orthogonal regularize penalty. Default: ``0.``
         theta (float, optional): coefficient used for weighing relative 
             contribution of past gradient and current gradient. (Default: ``(1.0)``)
+        normalize (bool, optional): perform normalization for each component
+            after each iteration.
+        update_state (bool, optional): update the historical state based on current
+            gradient.
     """
 
-    def __init__(self, params, beta=1, l1_reg=0, l2_reg=0, orthogonal=0, theta=1):
+    def __init__(self, params, beta=1, l1_reg=0, l2_reg=0, orthogonal=0, theta=1, normalize=False, update_state=False):
         if not 0.0 <= l1_reg:
             raise ValueError("Invalid l1_reg value: {}".format(l1_reg))
         if not 0.0 <= l2_reg:
@@ -32,8 +36,14 @@ class BetaMu(Optimizer):
             raise ValueError("Invalid orthogonal value: {}".format(orthogonal))
         if not 0.0 <= theta <= 1.0:
             raise ValueError("Invalid theta parameter value: {}".format(theta))
+        if not normalize in [True, False]:
+            raise ValueError("Invalid normalize parameter value: {}".format(normalize))
+        if not update_state in [True, False]:
+            raise ValueError("Invalid update_state parameter value: {}".format(normalize))
+
         defaults = dict(beta=beta, l1_reg=l1_reg,
-                        l2_reg=l2_reg, orthogonal=orthogonal, theta=theta)
+                        l2_reg=l2_reg, orthogonal=orthogonal, theta=theta,
+                        normalize=normalize, update_state=update_state)
         super(BetaMu, self).__init__(params, defaults)
 
     @torch.no_grad()
@@ -63,6 +73,11 @@ class BetaMu(Optimizer):
             l2_reg = group['l2_reg']
             ortho = group['orthogonal']
             theta = group['theta']
+            normalize = group['normalize']
+            update_state = group['update_state']
+
+            # TODO: Warn that if theta < 1 and update_state is False that the
+            # gradients will not incorporate previous state information.
 
             # Iterate over model parameters within the group
             # if a gradient is not required then that parameter is "fixed"
@@ -116,10 +131,6 @@ class BetaMu(Optimizer):
                 if ortho > 0:
                     pos.add_(p.sum(1, keepdims=True) - p, alpha=ortho)
 
-                # Avoid ill-conditioned, zero-valued multipliers
-                pos.add_(eps)
-                neg.add_(eps)
-
                 # Cache the multiplicative update numerator/denominator as state
                 # variables within the optimizer. Enables incremental learning.
                 # TODO: Lazy state initialization, should init in constructor
@@ -130,16 +141,18 @@ class BetaMu(Optimizer):
                     state['pos'] = torch.zeros_like(p, memory_format=torch.preserve_format)
 
                 # Accumulate gradients 
-                state['neg'] = (1-theta)*state['neg'] + (theta)*neg
-                state['pos'] = (1-theta)*state['pos'] + (theta)*pos
-                state['step'] += 1
+                neg = (1-theta)*state['neg'] + (theta)*neg
+                pos = (1-theta)*state['pos'] + (theta)*pos
+
+                # Avoid ill-conditioned, zero-valued multipliers
+                pos.add_(eps)
+                neg.add_(eps)
 
                 # Compute the current state of the parameter
-                p.mul_(state['neg'] / state['pos'])
+                p.mul_(neg / pos)
 
-                # If gradients are accumulated, then ensure the parameters
-                # are normalized and the gradients are also re-scaled
-                if theta < 1:
+                # If normalize then rescale based on beta-divgerence
+                if normalize:
 
                     # TODO: Iterator should reference a rank parameter rather than
                     # matrix shape.
@@ -149,8 +162,14 @@ class BetaMu(Optimizer):
                         else:
                             norm = (p[:,r]**beta).sum()**(1/beta)
                         p[:,r] = p[:,r] / norm
-                        state['neg'][:,r] = state['neg'][:,r] / norm
-                        state['pos'][:,r] = state['pos'][:,r] * norm
+                        neg[:, r] = neg[:, r] / norm
+                        pos[:, r] = pos[:, r] * norm
+
+                if update_state:
+                    state['neg'] = neg
+                    state['pos'] = pos
+                    state['step'] += 1
+
                 p.requires_grad = False
 
         # Reinstate the grad status from before
